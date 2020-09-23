28f85ae1a1fce5a010ec43e32cd4658f7fd427fa
Alan Cesar Pires <alancpires01@gmail.com>
2020-09-23 16:44:30 -0300

    fix: bug

--- src/components/typography/index.tsx
+++ src/components/typography/index.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react'
 
 interface Props {
-  value: number
+  value?: number
+  label?: string
 }
 
-const Typography = ({ value }: Props) => {
-  return <div>{value}</div>
+const Typography = ({ value, label }: Props) => {
+    const header = () => {
+        switch (value) {
+            case 1:
+                return "h1"
+            case 2:
+                return "h2"
+            case 3:
+                return "h3"
+            case 4:
+                return "h4"
+            case 5:
+                return "h5"
+            case 6:
+                return "h6"
+            default:
+                return "h1"
+        }
+    }
+
+  return <div>{label}</div>
 }
 
 export default Typography
