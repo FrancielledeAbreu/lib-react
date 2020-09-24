8df35c03e94cee0ba8bf027bb4be4431c50d0f74
Alan Cesar Pires <alancpires01@gmail.com>
2020-09-24 12:49:53 -0300

    feat: code refactor

--- src/components/typography/__tests__/index.test.jsx
+++ src/components/typography/__tests__/index.test.jsx
@@ -1,9 +1,61 @@
 import React from 'react'
 import Typography from '../index'
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 
-it("renders correctly", () => {
-    const tree = renderer.create(<Typography value="" label/>).toJSON()
+describe('Snapshot', () => {
+  it('renders correctly when the value is 1', () => {
+    const tree = renderer
+      .create(<Typography value={1} label='Teste' />)
+      .toJSON()
 
     expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly when the value is 2', () => {
+    const tree = renderer
+      .create(<Typography value={2} label='Teste' />)
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly when the value is 3', () => {
+    const tree = renderer
+      .create(<Typography value={3} label='Teste' />)
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly when the value is 4', () => {
+    const tree = renderer
+      .create(<Typography value={4} label='Teste' />)
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly when the value is 5', () => {
+    const tree = renderer
+      .create(<Typography value={5} label='Teste' />)
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly when the value is 6', () => {
+    const tree = renderer
+      .create(<Typography value={6} label='Teste' />)
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('does not render higher than more six', () => {
+    const tree = renderer
+      .create(<Typography value={7} label='Teste' />)
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
 })
--- src/components/typography/index.tsx
+++ src/components/typography/index.tsx
@@ -1,32 +1,17 @@
-import * as React from 'react'
-import {StyledDiv} from './style'
+import React from 'react'
+import { StyledDiv } from './style'
 
 interface Props {
-  value?: number
+  value: number
   label?: string | React.ReactNode
 }
 
 const Typography = ({ value, label }: Props) => {
-  const header = () => {
-    switch (value) {
-      case 1:
-        return <h1>{label}</h1>
-      case 2:
-        return <h2>{label}</h2>
-      case 3:
-        return <h3>{label}</h3>
-      case 4:
-        return <h4>{label}</h4>
-      case 5:
-        return <h5>{label}</h5>
-      case 6:
-        return <h6>{label}</h6>
-      default:
-        return <h6>{label}</h6>
-    }
-  }
+  const Result = React.createElement(`h${value > 6 ? 6 : value}`, {
+    children: label
+  })
 
-  return <StyledDiv>{header()}</StyledDiv>
+  return <StyledDiv>{Result}</StyledDiv>
 }
 
 export default Typography
