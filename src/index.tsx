ebc7338b0638f98552ff503ac994d2d7a8826779
llucasduartee <lucasduartedsvp@gmail.com>
2020-09-25 13:20:44 -0300

    done

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,3 @@
-import Input from './components/input/index'
-import Typography from './components/typography/index'
 import Modal from './components/modal/index'
 
-export { Typography }
-export { Input }
 export { Modal }
